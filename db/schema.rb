40796f30ca201e71998b53fe033e5df6fd707c4f
Ewelina Sarad <ewelina.sarad@lunarlogic.io>
2021-11-02 17:24:16 +0100

    Add gem paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_145433) do
+ActiveRecord::Schema.define(version: 2021_11_02_155410) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
